21aa60db9d538e257eb17be40cdf8813e23f765b
Cristian F Ritter <cristianritter@gmail.com>
2020-12-12 16:59:00 -0300

    salvando arquivos de hora completa

--- audiorecorder.py
+++ audiorecorder.py
@@ -1,66 +1,30 @@
 #!/usr/bin/python
 import pyaudio
 import struct
-import math
 import wave
 import time
-import io
 import parse_config
 import os
 import audioop
+from datetime import date, datetime
+import subprocess
+import shutil
+import sox
 
 configuration = parse_config.ConfPacket()
 configs = configuration.load_config('FILES, AUDIO_PARAM, ZABBIX, DETECTION_PARAM')
+saved_files_folder = os.path.join(configs['FILES']['saved_files_folder'])
+temp_file = os.path.join(configs['FILES']['temp_folder'],'temp.wav')
 
 FORMAT = pyaudio.paInt16 
-SHORT_NORMALIZE = (1.0/32768.0)
 CHANNELS = int(configs['AUDIO_PARAM']['channels'])
 RATE = int(configs['AUDIO_PARAM']['rate'])  
 INPUT_BLOCK_TIME = int(configs['AUDIO_PARAM']['input_block_time'])
-INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
-
-def get_rms_mono (block, ch1, ch2):
-    data = audioop.tomono(block, 2, ch1, ch2)
-    rms = audioop.rms(data, 2)
-    return rms
-
-def get_if_clipped (block):
-    count = len(block)/2
-    format = "%dh"%(count)
-    shorts = struct.unpack( format, block )
-    maximo = (max(shorts))
-    soma = 0
-    for sample in shorts:
-        # sample is a signed short in +/- 32768. 
-        # normalize it to 1.0
-        if abs(sample) == 32768:
-            soma += 1
-    retorno = {}
-    retorno['maximo'] = maximo
-    retorno['clippes'] = soma
-    return retorno
 
 class AudioRec(object):
     def __init__(self):
         self.pa = pyaudio.PyAudio()
         self.stream = self.open_mic_stream()
-
-    def find_input_device(self):
-        device_index = None            
-        for i in range( self.pa.get_device_count() ):     
-            devinfo = self.pa.get_device_info_by_index(i)   
-            print( "Device %d: %s"%(i,devinfo["name"]) )
-
-            for keyword in ["mic","input"]:
-                if keyword in devinfo["name"].lower():
-                    print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
-                    device_index = i
-                    return device_index
-
-        if device_index == None:
-            print( "No preferred input found; using default input device." )
-
-        return device_index
 
     def open_mic_stream( self ):
         device_index = self.find_input_device()
@@ -70,32 +34,94 @@ class AudioRec(object):
                                  rate = RATE,
                                  input = True,
                                  input_device_index = device_index,
-                                 frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
+                                 frames_per_buffer = RATE*INPUT_BLOCK_TIME)
         return stream     
 
-    def save_to_file( self, block ):
-        temp_file = os.path.join(configs['FILES']['temp_folder'],'temp.wav')
+    def find_input_device(self):
+        device_index = int(configs['AUDIO_PARAM']['device_index'])
+        devices_file = os.path.join(parse_config.ROOT_DIR, 'devices.txt')   
+        f = open(devices_file, "w")
+        for i in range( self.pa.get_device_count() ):     
+            devinfo = self.pa.get_device_info_by_index(i)   
+            for keyword in ["mic","input"]:
+                if keyword in devinfo["name"].lower():
+                    print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
+                    f.write("Found an input: device %d - %s"%(i,devinfo["name"]) )
+        f.close()
+        print( "Using device %d - %s"%(device_index,self.pa.get_device_info_by_index(device_index) ["name"]))
+        return device_index
+
+    def save_block_to_temp_file( self, block ):
         wf = wave.open(temp_file, 'wb')
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(2)
         wf.setframerate(RATE)
         wf.writeframes(block)
         wf.close()
-        time.sleep(1)
+        time.sleep(0.1)
 
+    def append_block_to_hour_file(self, block):
+        mes_ano_dia = datetime.now().strftime('%Y%m%d')
+        mes_ano_dia_hora = datetime.now().strftime('%Y%m%d_%H.mp3')
+        day_dir = os.path.join(configs['FILES']['temp_folder'],mes_ano_dia)
+        definitive_day_dir = os.path.join(configs['FILES']['saved_files_folder'],mes_ano_dia)
+        hour_file = os.path.join(day_dir, mes_ano_dia_hora)
+        definitive_hour_file = os.path.join(definitive_day_dir, mes_ano_dia_hora)
+
+        try:
+            os.mkdir(day_dir)
+        except:
+            pass
+        try:
+            shutil.copy(hour_file, hour_file+'.mp3')
+            subprocess.check_output('sox %s %s %s'
+                                    % (hour_file+'.mp3', temp_file, hour_file))
+        except:
+            subprocess.check_output('sox %s %s'
+                                    % (temp_file, hour_file))
+            pass
+        os.remove(hour_file+'.mp3')
+        comprimento = sox.file_info.stat(hour_file)
+        if ( float(comprimento['Length (seconds)']) > 3599 ):
+            os.mkdir(definitive_day_dir)
+            shutil.copy(hour_file, definitive_hour_file)
+            os.remove(hour_file)
+        
     def listen(self):
         try:
-            block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
+            block = self.stream.read(RATE*INPUT_BLOCK_TIME)
         except IOError as e:
             print( "Error recording: %s"%e )
             return
-        self.save_to_file(block)
-        gain = float(configs['DETECTION_PARAM']['ch1_gain'])
-        self.amplitude_l = get_rms_mono(block, gain, 0)/1000
-        self.amplitude_r = get_rms_mono(block, 0, 1)/1000
-        self.clipped = get_if_clipped(block)
+        self.channels_rms_lvl = self.get_rms(block)
+        self.clipped = self.get_if_clipped(block)
+        self.save_block_to_temp_file(block)
+        self.append_block_to_hour_file(block)
+        
+    def get_rms (self, block):
+        data_l = audioop.tomono(block, 2, 1, 0)
+        chL_rms = audioop.rms(data_l, 2)/1000
+        data_r = audioop.tomono(block, 2, 0, 1)
+        chR_rms = audioop.rms(data_r, 2)/1000
+        retorno = {}
+        retorno['L'] = chL_rms
+        retorno['R'] = chR_rms
+        return retorno
 
- 
+    def get_if_clipped (self, block):
+        count = len(block)/2
+        format = "%dh"%(count)
+        shorts = struct.unpack( format, block )
+        maximo = (max(shorts))
+        soma = 0
+        for sample in shorts:
+            if abs(sample) == 32768:
+                soma += 1
+        retorno = {}
+        retorno['maximo'] = maximo
+        retorno['clipped_count'] = soma
+        return retorno
+
 if __name__ == "__main__":
     tt = AudioRec()
     tt.listen()
--- fpcalc.py
+++ fpcalc.py
@@ -6,13 +6,12 @@ import os
 import time
 from threading import Thread
 from pyzabbix import ZabbixMetric, ZabbixSender
-import sys
 import sox
+
 
 #its better to create a ramdisk to use because rw disk stressfull
 
 temp_file = os.path.join(audiorecorder.configs['FILES']['temp_folder'],'temp.wav')
-temp_file_eq = os.path.join(audiorecorder.configs['FILES']['temp_folder'],'temp_eq.wav')
 temp_out_of_phase = os.path.join(audiorecorder.configs['FILES']['temp_folder'],'out_of_phase.wav')
 amplitude_min = float(audiorecorder.configs['DETECTION_PARAM']['silence_offset']) 
 stereo_min = float(audiorecorder.configs['DETECTION_PARAM']['stereo_offset'])
@@ -28,7 +27,6 @@ class Waiter(Thread):
 
 def is_stereo(filename):
     tfm = sox.Transformer()
-    tfm.set_globals(verbosity=0)
     tfm.oops()
     try:
         os.remove(temp_out_of_phase)
@@ -40,11 +38,7 @@ def is_stereo(filename):
          
 def compair_fingerprint(): 
     finger1 = calculate_fingerprints(os.path.join(audiorecorder.parse_config.ROOT_DIR, audiorecorder.configs['FILES']['sample_file']))
-    fpcalc_out2 = subprocess.check_output('sox %s %s highpass 2'
-                                    % (temp_file, temp_file_eq)).decode()
-    finger2 = calculate_fingerprints(temp_file_eq)
-    time.sleep(0.1)
-    
+    finger2 = calculate_fingerprints(temp_file)
     soma = 0
     for idx, item in enumerate(finger1):
         cont = (bin(int(item) ^ int(finger2[idx])).count("1"))
@@ -53,7 +47,7 @@ def compair_fingerprint():
     return soma
 
 def calculate_fingerprints(filename):
-    fpcalc_out = subprocess.check_output('fpcalc -algorithm 5 -channels 2 -raw -length 5 %s'
+    fpcalc_out = subprocess.check_output('fpcalc -algorithm 5 -channels 2 -raw %s'
                                     % (filename)).decode()
     lista_fp = fpcalc_out[fpcalc_out.find('=', 12)+1:].split(',')
     lista_fp[len(lista_fp)-1]=lista_fp[len(lista_fp)-1][:9]
@@ -61,9 +55,11 @@ def calculate_fingerprints(filename):
 
 def adiciona_linha_log(texto):
     dataFormatada = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    mes_ano = datetime.now().strftime('_%Y%m')
     print(dataFormatada, texto)
+    print(mes_ano)
     try:
-        f = open(audiorecorder.configs['FILES']['log_file'], "a")
+        f = open(audiorecorder.configs['FILES']['log_file']+mes_ano+'.txt', "a")
         f.write(dataFormatada + " " + texto +"\n")
         f.close()
     except Exception as err:
@@ -81,9 +77,8 @@ def convert_to_mp3(wav_file, mp3_file):
 
 tt = audiorecorder.AudioRec()
 Waiter().start()
-metric = 3
+metric = 0
 double_test = 0
-double_time = ""
 
 while (1):
     try:
@@ -91,15 +86,10 @@ while (1):
         dataFormatada = datetime.now().strftime('%d%m%Y_%H%M%S.mp3')
         stereo = is_stereo(temp_file)
         soma = compair_fingerprint()
-        if ((tt.amplitude_l < amplitude_min) or (tt.amplitude_r < amplitude_min)):
-            print("Silence Detected - Ch1 lvl:{} Ch2 lvl: {}".format(tt.amplitude_l, tt.amplitude_r))
+        if ((tt.channels_rms_lvl['L'] < amplitude_min) or (tt.channels_rms_lvl['R'] < amplitude_min)):
+            print("Silence Detected - Ch1 lvl:{} Ch2 lvl: {}".format(tt.channels_rms_lvl['L'], tt.channels_rms_lvl['R']))
             if (metric != 1):
-                if tt.amplitude_l < amplitude_min and tt.amplitude_l < amplitude_min:
-                    adiciona_linha_log("Both channel - Silencio L+R:{}".format (tt.amplitude_l+tt.amplitude_r))
-                elif tt.amplitude_l < amplitude_min:                
-                    adiciona_linha_log("Ch1 Amplitude: {}, - Silencio".format(tt.amplitude_l))
-                elif tt.amplitude_r < amplitude_min:                
-                    adiciona_linha_log("Ch2 Amplitude: {}, - Silencio".format(tt.amplitude_r))
+                adiciona_linha_log("Silence Detected - Ch1 lvl:{} Ch2 lvl: {}".format(tt.channels_rms_lvl['L'], tt.channels_rms_lvl['R']))
                 metric = 1
                 send_status_metric(metric)       
         
@@ -108,34 +98,35 @@ while (1):
             
             if metric != 2 and double_test == 0:
                 shutil.copy(temp_file, temp_doubt)
-                double_name = dataFormatada
+                doubt_name = dataFormatada
                 double_test = 1
 
             elif double_test == 1:
                 adiciona_linha_log("Fora do Ar by stereo comparation {} and fingerprint {}".format(stereo,soma))
+                dest_doubt = os.path.join(audiorecorder.configs['FILES']['saved_files_folder'], doubt_name)
+                convert_to_mp3(temp_doubt, dest_doubt)
                 metric = 2
                 send_status_metric(metric)
                 double_test = 0
 
-        elif (tt.clipped['clippes'] > 100):
+        elif (tt.clipped['clipped_count'] > 100):
             print("Clipped audio in {} samples".format(tt.clipped))
             if metric != 3:
                 adiciona_linha_log("Problemas no AR by clipped counting {}".format(tt.clipped['clippes']))
                 metric = 3
                 send_status_metric(metric)
-                
-            
+                 
         else:
             double_test = 0
-            print("On Air Ch1 lvl:{} Ch1 lvl:{} stereo:{} fingerprint:{}".format(tt.amplitude_l, tt.amplitude_r, stereo, soma))
+            print("On Air Ch1 lvl:{} Ch1 lvl:{} stereo:{} fingerprint:{}".format(tt.channels_rms_lvl['L'], tt.channels_rms_lvl['R'], stereo, soma))
             if (metric != 0):
-                adiciona_linha_log("On Air Ch1 lvl:{} Ch1 lvl:{} stereo:{} fingerprint:{}".format(tt.amplitude_l, tt.amplitude_r, stereo, soma))
+                adiciona_linha_log("On Air Ch1 lvl:{} Ch1 lvl:{} stereo:{} fingerprint:{}".format(tt.channels_rms_lvl['L'], tt.channels_rms_lvl['R'], stereo, soma))
                 metric = 0
+ 
         if metric != 0:
             dest_file = os.path.join(audiorecorder.configs['FILES']['saved_files_folder'], dataFormatada)
             convert_to_mp3(temp_file, dest_file)
         
-
     except Exception as err:
         print (err)
 
